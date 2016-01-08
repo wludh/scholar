f1888fcfc9265eedc32d0b3cea8ed0935e2870d1
Brandon Walsh <walshb@wlu.edu>
2016-01-08 06:52:22 -0500

    Refs #9 - Initial pass at filtering by S/R categories.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,7 +10,7 @@ class AdminController < ApplicationController
     @works = Work.get_works_by_sherpa(params).paginate(:page => params[:page], :per_page => 10)
       respond_to do |format|
       format.html # index.html.erb
-      format.json { render :json => @works }
+      #format.json { render :json => @works }
     end
   end
 
