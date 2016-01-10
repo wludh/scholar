b9357738e10edb051af4cae1e31c6a7fed5fbca2
Brandon Walsh <walshb@wlu.edu>
2016-01-10 14:42:33 -0500

    forces encoding on the value of the facet in solr. note that this does not actually change the data itself, just the encoding. So it could potentially cause wonky things to happen elsewhere. But since the helper only gets called in SOLR and I'm only modifying the encoding internal to the helper it should be isolated enough that it doesn't cause problems. For more information check out http://graysoftinc.com/character-encodings/ruby-19s-string

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -55,6 +55,8 @@ module SharedHelper
 
   def add_filter(params, facet, value, count, label = nil)
     label ||= value
+    #facet = facet.force_encoding("UTF-8")
+    value = value.force_encoding("UTF-8")
     filter = Hash.new
     if params[:fq]
       filter[:fq] = params[:fq].collect.to_a
